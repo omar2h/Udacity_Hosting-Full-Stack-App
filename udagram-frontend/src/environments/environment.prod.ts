2ed1f38f7213cb5741600f3ca03d6271d407db06
omarsamak <omarhesham83@gmail.com>
2022-03-27 17:38:29 +0200

    fix: replace app links

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://localhost:3000/api/v0",
+  apiHost: "http://udagram-api-dev.eba-rinype73.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
