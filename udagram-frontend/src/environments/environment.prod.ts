43e76020bb8e96e4ce371261236c7a1293ad8295
omarsamak <omarhesham83@gmail.com>
2022-03-28 15:07:47 +0200

    fix: edit links

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: 'http://udagramapi-env.eba-uuhzarrn.us-east-1.elasticbeanstalk.com/api/v0',
+  apiHost: 'http://udagram-api2-env.eba-pqnfnyrm.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
