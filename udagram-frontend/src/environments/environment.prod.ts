b24248a903cf5ab83da0c7fe6fb0f417860ca1de
omarsamak <omarhesham83@gmail.com>
2022-03-29 16:00:46 +0200

    fix: new aws

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: 'http://udagram-api2-env.eba-pqnfnyrm.us-east-1.elasticbeanstalk.com/api/v0',
+  apiHost: 'http://udagramapi-env.eba-z3gysyck.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://udagram-api2-env.eba-pqnfnyrm.us-east-1.elasticbeanstalk.com/api/v0'
+  apiHost: 'http://udagramapi-env.eba-z3gysyck.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
